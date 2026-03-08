0cf7d79a9847845c8813ac2b1fb90389a26c0046
Ankit Gujre <ankitgujre374@gmail.com>
2026-03-08 17:27:36 +0530

    stack implementation using class

--- StackDSA.py
+++ StackDSA.py
@@ -3,17 +3,17 @@
 
 # list implementation of stack
 
-li1 = []
+# li1 = []
 
-li1.append(5)
-li1.append(8)
-li1.append(56)
-li1.append(50)
-li1.append(20)
-print(li1)
+# li1.append(5)
+# li1.append(8)
+# li1.append(56)
+# li1.append(50)
+# li1.append(20)
+# print(li1)
 
-li1.pop()
-print(li1)
+# li1.pop()
+# print(li1)
 
 class stack:
     def __init__(self):
@@ -22,4 +22,27 @@ class stack:
     def length(self):
         return len(self.s)
     
+    def push(self, value):
+        self.s.insert(0, value)
+
+    def peek(self):       # dekhne ke liye
+        if len(self.s) == 0:
+            raise Exception("stack is empty")
+        else:
+            return self.s[0]
+    def pop(self):
+        return self.s.pop(0)  # index 0
     
+stk = stack()
+stk.push(55)
+stk.push(85)
+stk.push(5)
+
+print(stk.peek())   # last index dekhne ke liye
+# print(stk())
+
+print(stk.peek())
+
+print(stk.pop())
+
+print(stk.s)
